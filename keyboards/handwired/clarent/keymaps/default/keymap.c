8c41e3d2fa573a734decb81d775696827bb809ae
unknown <andrewhytai@gmail.com>
2025-05-14 00:15:16 -0400

    add rev 2

--- keyboards/handwired/clarent/keymaps/default/keymap.c
+++ keyboards/handwired/clarent/keymaps/default/keymap.c
@@ -31,7 +31,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                     KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,              KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_BSLS,
     MO(_LOWER),     KC_ESC,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,              KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_QUOT,
     KC_LSFT,        KC_LCTL,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,              XXXXXXX, KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  RSFT_T(KC_ENT),
-                            KC_LALT,    KC_SPC,   KC_LSFT, KC_LGUI,                   LT(_RTHUMB, KC_SPC),  MO(_RAISE), XXXXXXX, XXXXXXX, XXXXXXX
+                            KC_LALT,    KC_SPC,   KC_LSFT, KC_LGUI,                   LT(_RTHUMB, KC_SPC),  MO(_RAISE), KC_A, KC_B, KC_C
     ),
     [_RAISE] = LAYOUT(
                 KC_F1,   KC_F2,   KC_F3,    KC_F4,    KC_F5,    KC_F6,             KC_F7,    KC_F8,    KC_F9,    KC_F10,    KC_F11,  KC_F12,
@@ -60,6 +60,6 @@ void keyboard_post_init_user(void) {
   // Customise these values to desired behaviour
   debug_enable=true;
   debug_matrix=true;
-  //debug_keyboard=true;
+  debug_keyboard=true;
   //debug_mouse=true;
 }
